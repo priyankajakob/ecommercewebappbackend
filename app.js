d44ec14dcc02de02b38abaa95b8851926b658299
priyankajakob <priyanka.jacob93@gmail.com>
2020-11-14 22:45:47 +0530

    stripe integrated

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 const express = require('express')
-const mongoose = require('./config/database')
+const mongoose = require('./config/database') //required to make db connection
 const bodyParser = require('body-parser')
-const cookieParser = require('cookie-parser')
+// const cookieParser = require('cookie-parser')
 const cors = require('cors')
 
-const authRoutes = require('../projbackend/routes/auth')
-const userRoutes = require('../projbackend/routes/user')
-const categoryRoutes = require('../projbackend/routes/category')
-const productRoutes = require('../projbackend/routes/product')
-const orderRoutes = require('../projbackend/routes/order')
+const authRoutes = require('./routes/auth')
+const userRoutes = require('./routes/user')
+const categoryRoutes = require('./routes/category')
+const productRoutes = require('./routes/product')
+const orderRoutes = require('./routes/order')
+const stripeRoutes = require('./routes/stripe')
 
 const app = express()
 
@@ -25,6 +26,7 @@ app.use('/',userRoutes)
 app.use('/',categoryRoutes)
 app.use('/',productRoutes)
 app.use('/',orderRoutes)
+app.use('/',stripeRoutes)
 
 //starting server
 app.listen(PORT,()=>{
---
+++ controllers/stripeController.js
@@ -0,0 +1,41 @@
+const stripe = require("stripe")("sk_test_")
+const uuid = require("uuid/v4")
+
+module.exports.makePayment = (req,res)=>{
+    const {products , token} = req.body
+    console.log("PRODUCTS : ",products)
+
+    let amount = 0
+    products.forEach((prod)=>{
+        amount += prod.count * prod.price
+    })
+
+    const idempotencyKey = uuid() //so that user is not chanrged twice in case of network issues
+
+    return stripe.customers.create({
+        email : token.email,
+        source: token.id
+    })
+    .then((customer)=>{
+        stripe.charges.create({
+            amount : amount,
+            currency : 'inr',
+            customer:customer.id,
+            receipt_email:token.email,
+            shipping : {
+                name:token.card.name,
+                address : {
+                    line1:token.card.address_line1,
+                    line2:token.card.address_line2,
+                    city:token.card.address_city,
+                    country:token.card.address_country,
+                    postal_code:token.card.address_zip
+                }
+            }
+
+        },{idempotencyKey})
+        .then((result)=>res.status(200).json(result))
+        .catch((error)=>console.log(error))
+    })
+    .catch((error)=>console.log(error))
+}
